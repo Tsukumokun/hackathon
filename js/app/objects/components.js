b96f3c3315a145cddea199ea32d25000deaa335a
Till Smejkal <till.smejkal@mailbox.tu-dresden.de>
2014-02-17 16:37:54 -0500

    Changed base property of subclasses of Component to the correct type
    All subclasses of the Component object had the wrong base property. They
    are changed to the Component object to represent the inheritance tree
    correctly.

--- js/app/objects/components.js
+++ js/app/objects/components.js
@@ -2,17 +2,17 @@
 // Hackathon - a web based fight and collect game.
 // Copyright (C) 2014 Christopher Kelley   <tsukumokun(at)icloud.com>
 //                    Till Smejkal         <till.smejkal(at)gmail.com>
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http:// www.gnu.org/licenses/>
 //
@@ -31,25 +31,25 @@ Component.prototype = new Base;
 
 
 function Pipe(){
-	this.base = Base;
+	this.base = Component;
     this.base();
 }
 Pipe.prototype = new Component;
 
 function Cpu(){
-	this.base = Base;
+	this.base = Component;
     this.base();
 }
 Cpu.prototype = new Component;
 
 function Memory(){
-	this.base = Base;
+	this.base = Component;
     this.base();
 }
 Memory.prototype = new Component;
 
 function Hdd(){
-	this.base = Base;
+	this.base = Component;
     this.base();
 }
 Hdd.prototype = new Component;
