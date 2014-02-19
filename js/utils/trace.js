87a7f9a44b5174ad995515b05b58ae2b831980d7
Tsukumo <tsukumokun@icloud.com>
2014-02-18 20:49:50 -0500

    Added enabling and disabling of traces at runtime via trace_[type]_(en|dis)able() from the console

--- js/utils/trace.js
+++ js/utils/trace.js
@@ -19,11 +19,23 @@
 
 //REQUIRE: browser.js
 
-function getErrorObject(){
-    try { throw Error('') } catch(err) {return err;}
-}
+
+
+// Override the console directive 
+//  This effectively allows trace to manage
+//  logging from third party scripts
 
 var internal_trace_log = console.log.bind(console);
+
+var console    = {};
+console.log    = function(message){TEXT(message);};
+console.info   = function(message){INFO(message);};
+console.warn   = function(message){WARN(message);};
+console.error  = function(message){ERROR(message);};
+
+var getErrorObject = function(){
+    try { throw Error('') } catch(err) {return err;}
+}
 
 var trace_log = function(type,message,css){
 	internal_trace_log("Unsupported Browser: "+type+":"+message);
@@ -86,6 +98,8 @@ else if(BROWSER_FIREFOX){
 	}
 }
 
+//Setup trace toggles
+
 var ERROR   = function(){};
 var SEVERE  = function(){};
 var WARN    = function(){};
@@ -96,7 +110,8 @@ var TODO    = function(){};
 var XXX     = function(){};
 var TEXT    = function(){};
 
-if(typeof RELEASE === 'undefined'){
+function trace_all_enable()
+{
 	ERROR   = function(message){trace_log("[ERROR] ",message,'color: #E74C3C');}
 	SEVERE  = function(message){trace_log("[SEVERE]",message,'color: #C0392B');}
 	WARN    = function(message){trace_log("[WARN]  ",message,'color: #F1C40F');}
@@ -106,26 +121,112 @@ if(typeof RELEASE === 'undefined'){
 	TODO    = function(message){trace_log("[TODO]  ",message,'color: #2ECC71');}
 	XXX     = function(message){trace_log("[XXX]   ",message,'color: #1ABC9C');}
 	TEXT    = function(message){trace_log("[TEXT]  ",message,'color: #34495E');}
-
-	SYSTEM("Tracing Enabled");
-	//var console = {};
-	//console.warn = function(message){WARN(message);};
-	//console.err  = function(message){ERROR(message);};
+	internal_trace_log("All Tracing Enabled");
 }
-else
+
+function trace_all_disable()
 {
-	console.log("Tracing Disabled for Release");
+	ERROR   = function(){};
+	SEVERE  = function(){};
+	WARN    = function(){};
+	INFO    = function(){};
+	SYSTEM  = function(){};
+	NOTE    = function(){};
+	TODO    = function(){};
+	XXX     = function(){};
+	TEXT    = function(){};
+	internal_trace_log("All Tracing Disabled");
 }
 
+function trace_error_enable()
+{
+	ERROR   = function(message){trace_log("[ERROR] ",message,'color: #E74C3C');}
+	internal_trace_log("Error Tracing Enabled");
+}
+function trace_error_disable()
+{
+	ERROR   = function(){};
+	internal_trace_log("Error Tracing Disabled");
+}
+function trace_severe_enable()
+{
+	SEVERE  = function(message){trace_log("[SEVERE]",message,'color: #C0392B');}
+	internal_trace_log("Severe Tracing Enabled");
+}
+function trace_severe_disable()
+{
+	SEVERE  = function(){};
+	internal_trace_log("Severe Tracing Disabled");
+}
+function trace_warn_enable()
+{
+	WARN    = function(message){trace_log("[WARN]  ",message,'color: #F1C40F');}
+	internal_trace_log("Warn Tracing Enabled");
+}
+function trace_warn_disable()
+{
+	WARN    = function(){};
+	internal_trace_log("Warn Tracing Disabled");
+}
+function trace_info_enable()
+{
+	INFO    = function(message){trace_log("[INFO]  ",message,'color: #3498DB');}
+	internal_trace_log("Info Tracing Enabled");
+}
+function trace_info_disable()
+{
+	INFO    = function(){};
+	internal_trace_log("Info Tracing Disabled");
+}
+function trace_system_enable()
+{
+	SYSTEM  = function(message){trace_log("[SYSTEM]",message,'color: #9B59B6');}
+	internal_trace_log("System Tracing Enabled");
+}
+function trace_system_disable()
+{
+	SYSTEM  = function(){};
+	internal_trace_log("System Tracing Disabled");
+}
+function trace_note_enable()
+{
+	NOTE    = function(message){trace_log("[NOTE]  ",message,'color: #95A5A6');}
+	internal_trace_log("Note Tracing Enabled");
+}
+function trace_note_disable()
+{
+	NOTE    = function(){};
+	internal_trace_log("Note Tracing Disabled");
+}
+function trace_todo_enable()
+{
+	TODO    = function(message){trace_log("[TODO]  ",message,'color: #2ECC71');}
+	internal_trace_log("Todo Tracing Enabled");
+}
+function trace_todo_disable()
+{
+	TODO    = function(){};
+	internal_trace_log("Todo Tracing Disabled");
+}
+function trace_xxx_enable()
+{
+	XXX     = function(message){trace_log("[XXX]   ",message,'color: #1ABC9C');}
+	internal_trace_log("XXX Tracing Enabled");
+}
+function trace_xxx_disable()
+{
+	XXX     = function(){};
+	internal_trace_log("XXX Tracing Disabled");
+}
+function trace_text_enable()
+{
+	TEXT    = function(message){trace_log("[TEXT]  ",message,'color: #34495E');}
+	internal_trace_log("Text Tracing Enabled");
+}
+function trace_text_disable()
+{
+	TEXT    = function(){};
+	internal_trace_log("Text Tracing Disabled");
+}
 
-//TODO: make this more robust
-var console    = {};
-console.log    = function(message){TEXT(message);};
-console.info   = function(message){INFO(message);};
-console.warn   = function(message){WARN(message);};
-console.error  = function(message){ERROR(message);};
-
-
-
-
-
+trace_all_enable();
